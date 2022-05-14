3c8d4a91766a0450eedcec763e979211ff2b1776
Alysson Victor <alysson@pop-os.localdomain>
2022-05-14 19:20:43 -0300

    Add Operation abstract class

---
+++ operations/Operation.java
@@ -0,0 +1,26 @@
+package operations;
+
+public abstract class Operation extends Values {
+
+    protected String operationSymbol;
+
+    protected int operationNumber;
+
+    public String getOperationSymbol() {
+        return operationSymbol;
+    }
+
+    public void setOperationSymbol(String operationSymbol) {
+        this.operationSymbol = operationSymbol;
+    }
+
+    public int getOperationNumber() {
+        return operationNumber;
+    }
+
+    public void setOperationNumber(int operationNumber) {
+        this.operationNumber = operationNumber;
+    }
+
+    public abstract void calc(double a, double b);
+}
