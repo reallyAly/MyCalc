8978b7bc063ba882e6c37073c0a1478ecff1a1e0
Alysson Victor <alysson@pop-os.localdomain>
2022-05-15 13:16:35 -0300

    Add calculator class

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+import calculator.BaseCalculator;
+import operations.Operations;
+
+public class Calculator extends BaseCalculator {
+
+    public Calculator(Operations operations) {
+        this.operations = operations;
+    }
+
+}
