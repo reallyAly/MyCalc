35786b3866beefe29170f3fd06a8d695b16f0433
Alysson Victor <alysson@pop-os.localdomain>
2022-05-15 13:13:36 -0300

    Update Operations class adding a init value on operations attribute

--- operations/Operations.java
+++ operations/Operations.java
@@ -5,7 +5,7 @@ import operations.Operation;
 import java.util.ArrayList;
 
 public class Operations {
-    private ArrayList<Operation> operations;
+    private ArrayList<Operation> operations = new ArrayList<>();
 
     public ArrayList<Operation> getOperations() {
         return operations;
