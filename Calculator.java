3cf654efb769bd50c31999b959408abf67e15646
Alysson Victor <alysson@pop-os.localdomain>
2022-05-16 10:59:11 -0300

    Change logic of calculator from Main to Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,41 @@
 import calculator.BaseCalculator;
+import operations.Operation;
 import operations.Operations;
+
+import java.util.Scanner;
 
 public class Calculator extends BaseCalculator {
 
     public Calculator(Operations operations) {
         this.operations = operations;
+    }
+
+    public void executeCalculator() {
+        Scanner reader = new Scanner(System.in);
+
+        int flag = 0;
+
+        do{
+            System.out.println("Insira o primeiro valor");
+            double firstValue = reader.nextDouble();
+
+            System.out.println("Insira o operador desejado");
+
+            for (Operation operation : this.operations.getOperations()) {
+                System.out.println(operation.getOperationName()+" => "+operation.getOperationSymbol());
+            }
+
+            this.setOperator(reader.next());
+
+            System.out.println("Insira o segundo valor");
+            double secondValue = reader.nextDouble();
+
+            this.calculate(firstValue, secondValue);
+
+            System.out.println("Deseja realizar outra operação? 1 - SIM | 0 - SAIR");
+            flag = reader.nextInt();
+
+        }while(flag == 1);
     }
 
 }
--- Main.java
+++ Main.java
@@ -10,8 +10,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner reader = new Scanner(System.in);
-
         Operations op = new Operations();
 
         op.addOperation(new Sum());
@@ -21,29 +19,7 @@ public class Main {
 
         Calculator calc = new Calculator(op);
 
-        int flag = 0;
-
-        do{
-            System.out.println("Insira o primeiro valor");
-            double firstValue = reader.nextDouble();
-
-            System.out.println("Insira o operador desejado");
-
-            for (Operation operation : op.getOperations()) {
-                System.out.println(operation.getOperationName()+" => "+operation.getOperationSymbol());
-            }
-
-            calc.setOperator(reader.next());
-
-            System.out.println("Insira o segundo valor");
-            double secondValue = reader.nextDouble();
-
-            calc.calculate(firstValue, secondValue);
-
-            System.out.println("Deseja realizar outra operação? 1 - SIM | 0 - SAIR");
-            flag = reader.nextInt();
-
-        }while(flag == 1);
+        calc.executeCalculator();
 
     }
 
