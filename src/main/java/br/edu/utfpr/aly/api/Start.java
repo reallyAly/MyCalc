4f24230ef899775a3c11efd1d7e713cf2c45826c
Alysson Victor <alyssonsantos@alunos.utfpr.edu.br>
2022-06-18 21:05:29 -0300

    Add POST calculate endpoint without return result, only show on console

--- src/main/java/br/edu/utfpr/aly/api/Start.java
+++ src/main/java/br/edu/utfpr/aly/api/Start.java
@@ -1,15 +1,19 @@
 package br.edu.utfpr.aly.api;
 
+import br.edu.utfpr.aly.Calculator;
 import br.edu.utfpr.aly.operations.Operation;
 import io.javalin.Javalin;
 import br.edu.utfpr.aly.operations.Operations;
 import br.edu.utfpr.aly.operations.operation.*;
-import br.edu.utfpr.aly.Calculator;
+import br.edu.utfpr.aly.api.utils.ConvertJson;
+import br.edu.utfpr.aly.api.Data;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Start {
+
     public static void main(String[] args) {
 
         Operations operations = new Operations();
@@ -18,6 +22,8 @@ public class Start {
         operations.addOperation(new Subtract());
         operations.addOperation(new Multiply());
         operations.addOperation(new Divide());
+
+        Calculator calc = new Calculator(operations);
 
         Javalin app = Javalin.create().start(7070);
 
@@ -31,6 +37,21 @@ public class Start {
             }
 
             ctx.json(resultOps);
+        });
+
+        /* Calculate and show result */
+        app.post("/calculate", ctx -> {
+
+            ConvertJson util = new ConvertJson();
+            Data data = util.getData(ctx.body());
+
+            ArrayList<Integer> numbers = data.getNumbers();
+
+            calc.setOperator(data.getOperator());
+            calc.calculate(
+                    Double.parseDouble(String.valueOf(numbers.get(0))),
+                    Double.parseDouble(String.valueOf(numbers.get(1)))
+            );
         });
     }
 }
